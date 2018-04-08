0c2d490140cdd614fc3189932e1988f1de31eb44
ifhammaf1905 <ifhammaf1905@bsi.ac.id>
2018-04-08 08:14:27 +0700

    Update safelink.js

--- safelink.js
+++ safelink.js
@@ -24,7 +24,7 @@ var xxx=null;
 if(x=="1"){xxx="the-reasons-which-trigger-liability.html"}
 if(x=="2"){xxx="what-makes-insurance-to-be-paradigm-for.html"}
 if(x=="3"){xxx="ocean-harbor-insurance-complete.html"}
-if(x=="4"){xxx="application-used-for-fastest-growing.html"}a_to_ve[i].href="http://clickdllink.blogspot.co.id/2018/04/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="4"){xxx="application-used-for-fastest-growing.html"}a_to_ve[i].href="https://www.clickdllink.ga/2018/04/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
